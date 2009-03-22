e4ee1971bf8986a4434192b5f72d4c139f5a181f
Thomas Paviot <tpaviot@gmail.com>
2009-03-22 06:42:14 +0000

    Fixes, GPLv3 header added

--- samples/TopologyOperations/cut.py
+++ samples/TopologyOperations/cut.py
@@ -1,3 +1,20 @@
+##Copyright 2009 Thomas Paviot (thomas.paviot@free.fr)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 from OCC.gp import *
 from OCC.BRepPrimAPI import *
 from OCC.BRepFilletAPI import *
@@ -9,24 +26,21 @@ from OCC.BRepAlgoAPI import *
 #
 # Create Box
 #
-Box = BRepPrimAPI_MakeBox(200,60,60)
-Box_s = Box.Shape()
+Box = BRepPrimAPI_MakeBox(200,60,60).Shape()
 #
 # Create Sphere
 #
-Sphere = BRepPrimAPI_MakeSphere(gp_Pnt(100,20,20),80)
-Sphere_s = Sphere.Shape()
+Sphere = BRepPrimAPI_MakeSphere(gp_Pnt(100,20,20),80).Shape()
 #
 # Cut: the shere is cut 'by' the box
 #
-Cut = BRepAlgoAPI_Cut(Sphere_s,Box_s)
-Cut_s= Cut.Shape()
+Cut = BRepAlgoAPI_Cut(Sphere,Box).Shape()
            
 if 'display' in dir():
     import time
     display.EraseAll()
-    display.DisplayShape(Box_s)
-    display.DisplayShape(Sphere_s)
+    display.DisplayShape(Box)
+    display.DisplayShape(Sphere)
     time.sleep(2)
     display.EraseAll()
-    display.DisplayShape(Cut_s)
+    display.DisplayShape(Cut)
--- samples/TopologyOperations/variable_filleting.py
+++ samples/TopologyOperations/variable_filleting.py
@@ -1,3 +1,20 @@
+##Copyright 2009 Thomas Paviot (thomas.paviot@free.fr)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 from OCC.gp import *
 from OCC.BRepPrimAPI import *
 from OCC.BRepAlgoAPI import *
@@ -10,13 +27,12 @@ import math
 #
 # Create Box
 #
-Box = BRepPrimAPI_MakeBox(200,200,200)
-Box_s = Box.Shape()
+Box = BRepPrimAPI_MakeBox(200,200,200).Shape()
 #
 # Fillet
 #
-Rake = BRepFilletAPI_MakeFillet(Box_s)      
-ex = TopExp_Explorer(Box_s,TopAbs_EDGE)
+Rake = BRepFilletAPI_MakeFillet(Box)      
+ex = TopExp_Explorer(Box,TopAbs_EDGE)
 ex.Next()
 ex.Next()
 ex.Next()
@@ -28,26 +44,23 @@ if Rake.IsDone():
 #
 # Create Cylinder
 #
-Cylinder = BRepPrimAPI_MakeCylinder(gp_Ax2(gp_Pnt(-300,0,0),gp_Dir(0,0,1)),100,200)
-Cylinder_s = Cylinder.Shape()
-fillet = BRepFilletAPI_MakeFillet(Cylinder_s)
+Cylinder = BRepPrimAPI_MakeCylinder(gp_Ax2(gp_Pnt(-300,0,0),gp_Dir(0,0,1)),100,200).Shape()
+fillet = BRepFilletAPI_MakeFillet(Cylinder)
 
-print "1"
 TabPoint2 = TColgp_Array1OfPnt2d(0,20)
 for i in range(0,20):
     Point2d = gp_Pnt2d(i*2*math.pi/19,60*math.cos(i*math.pi/19-math.pi/2)+10)
     TabPoint2.SetValue(i,Point2d)
 #    append(Point2d)
-exp2 = TopExp_Explorer(Cylinder_s,TopAbs_EDGE)
+exp2 = TopExp_Explorer(Cylinder,TopAbs_EDGE)
 fillet.Add(TabPoint2,TopoDS().Edge(exp2.Current()))
 fillet.Build()
 if fillet.IsDone():
     LawEvolvedCylinder = fillet.Shape()
-print "2"
+
 P = gp_Pnt(350,0,0)
-Box2 = BRepPrimAPI_MakeBox(P,200,200,200)
-Box2_s = Box2.Shape()
-afillet = BRepFilletAPI_MakeFillet(Box2_s)
+Box2 = BRepPrimAPI_MakeBox(P,200,200,200).Shape()
+afillet = BRepFilletAPI_MakeFillet(Box2)
 
 TabPoint = TColgp_Array1OfPnt2d(1,6)
 P1 = gp_Pnt2d(0.,8.)
@@ -63,7 +76,7 @@ TabPoint.SetValue(4,P4)
 TabPoint.SetValue(5,P5)
 TabPoint.SetValue(6,P6)
        
-exp = TopExp_Explorer(Box2_s,TopAbs_EDGE)
+exp = TopExp_Explorer(Box2,TopAbs_EDGE)
 exp.Next()
 exp.Next()
 exp.Next()
@@ -76,7 +89,7 @@ if afillet.IsDone():
 if 'display' in dir():
     import time
     display.EraseAll()
-    display.DisplayShape(Box_s)
+    display.DisplayShape(Box)
     time.sleep(2)
     display.EraseAll()
     display.DisplayShape(evolvedBox)    
