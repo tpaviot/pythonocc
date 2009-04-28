7f63fbe08fc53bd6e44989123b2d933576ca353c
Thomas Paviot <tpaviot@gmail.com>
2009-04-28 04:04:01 +0000

    Added pickling sample

---
+++ samples/Level1/Serialization/TopoDS_Shape_pickling.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+##Copyright 2009 Thomas Paviot (thomas.paviot@free.fr)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+from OCC.TopoDS import *
+from OCC.TopTools import *
+from OCC.BRepPrimAPI import *
+from OCC.BRepTools import *
+from OCC.BRep import *
+import pickle
+from OCC.Utils.Topology import Topo
+from OCC.TopLoc import *
+
+# Create shape
+box1 = BRepPrimAPI_MakeBox(10.,10.,10.).Shape()
+
+# Dump shape in a string
+box_dump_string = pickle.dumps(box1)
+
+#Create another box from the dump
+box2 = pickle.loads(box_dump_string)
+
