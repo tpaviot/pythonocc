43cbbc9755fd4cfca2f3fe06b328d7a232adc2ed
Thomas Paviot <tpaviot@gmail.com>
2009-04-28 04:02:53 +0000

    Added GPL headers

--- samples/Level2/XMLRPC/geometry_server.py
+++ samples/Level2/XMLRPC/geometry_server.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+
+##Copyright 2009 Thomas Paviot (thomas.paviot@free.fr)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 import SimpleXMLRPCServer
 from OCC.BRepPrimAPI import *
 from OCC.TopoDS import *
