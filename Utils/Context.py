f2f4428f2e6c21470b7401fbd6819cca3e234275
Jelle Feringa <jelleferinga@gmail.com>
2009-05-22 09:59:44 +0000

    added a assert_isdone context, to be used using the `with` statement

---
+++ Utils/Context.py
@@ -0,0 +1,16 @@
+class assert_isdone(object):
+    '''
+    raises an assertion error when IsDone() returns false, with the error specified in error_statement
+    '''
+    def __init__(self, to_check, error_statement):
+        self.to_check = to_check
+        self.error_statement = error_statement
+        
+    def __enter__(self, ):
+        if self.to_check.IsDone():
+            print 'done!'
+            pass
+        else:
+            raise AssertionError, self.error_statement
+    def __exit__(self, type, value, traceback):
+        pass
