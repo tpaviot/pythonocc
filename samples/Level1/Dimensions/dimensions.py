7bff263b704739e74c35cdf3a20fdcf50536c4b8
Jelle Feringa <jelleferinga@gmail.com>
2009-06-15 12:58:12 +0000

    fix dependency

--- samples/Level1/Dimensions/dimensions.py
+++ samples/Level1/Dimensions/dimensions.py
@@ -4,8 +4,10 @@ from OCC.AIS import *
 from OCC.TCollection import TCollection_ExtendedString
 
 from OCC.Display.wxSamplesGui import *
-from OCC.Utils.Construct import *
+from OCC.BRepBuilderAPI import *
 
+def make_edge(*args):
+    return BRepBuilderAPI_MakeEdge(*args).Edge()
 
 c = gp_Circ(gp_Ax2(gp_Pnt(200.,200.,0.),gp_Dir(0.,0.,1.)), 80)
 ec = make_edge(c)
@@ -13,6 +15,6 @@ ais7 = AIS_Shape(ec)
 display.Context.Display(ais7.GetHandle())
 rd = AIS_RadiusDimension(ec,80,TCollection_ExtendedString("radius"))
 display.Context.Display(rd.GetHandle())
-
+display.FitAll()
 start_display()
  
