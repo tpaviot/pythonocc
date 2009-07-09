85cfa45703cbeb6f2b03e8a47544188f8980b3d0
Thomas Paviot <tpaviot@gmail.com>
2009-07-09 21:56:33 +0000

    Added IGES tests

---
+++ samples/Level2/DataExchange/import_iges_multi.py
@@ -0,0 +1,26 @@
+##Copyright 2009 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+from OCC.Utils.DataExchange.IGES import IGESImporter
+from OCC.Display.wxSamplesGui import display, start_display
+
+my_iges_importer = IGESImporter("result_export_multi.iges")
+my_iges_importer.ReadFile()
+the_shapes = my_iges_importer.GetShapes()
+
+display.DisplayShape(the_shapes)
+start_display()
