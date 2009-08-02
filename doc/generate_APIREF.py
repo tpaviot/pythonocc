c0133d1133f5bd5ba61dc36762d25c5589ce144d
Thomas Paviot <tpaviot@gmail.com>
2009-08-02 21:48:00 +0000

    Refactoring: moved /generate_APIREF.py to /doc directory

---
+++ doc/generate_APIREF.py
@@ -0,0 +1,45 @@
+# -*- coding: iso-8859-1 -*-
+#! /usr/bin/python
+
+##Copyright 2008-2009 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# This script generates the API Reference using either pydoc or epydoc
+#
+import sys
+import os
+from environment import VERSION
+
+DOC_SYSTEM = "epydoc" #or "epydoc"
+
+if DOC_SYSTEM == "pydoc":
+    import pydoc
+    import OCC
+    modules = sys.modules
+    root_module = modules['OCC']
+    child_modules = dir(root_module)
+    print child_modules
+    pydoc.writedoc(OCC)
+    for module in child_modules:
+        if not module.startswith('_') and not(module=='VERSION'):        
+            md = modules[''.join(['OCC.',module])]
+            pydoc.writedoc(md)
+
+elif DOC_SYSTEM == "epydoc":
+    os.system('epydoc.py OCC -o ./APIREF --html --no-sourcecode  --graph all -v --name="pythonOCC %s API reference"'%VERSION)
+
