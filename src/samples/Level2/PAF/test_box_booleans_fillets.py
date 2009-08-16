59b4c03892bd632ab305629241f2005af8f9f913
Jelle Feringa <jelleferinga@gmail.com>
2009-08-16 11:54:51 +0000

    for testing PAF performance!

--- src/samples/Level2/PAF/test_box_booleans_fillets.py
+++ src/samples/Level2/PAF/test_box_booleans_fillets.py
@@ -1,8 +1,10 @@
 from OCC.PAF.Context import Context
 from OCC.PAF.Parametric import Parameters
 
+import time
+
 p = Parameters()            # Create a parameters set
-my_context = Context(p)     # Create and initialize a parametric context
+my_context = Context(p, commit=False)     # Create and initialize a parametric context
 my_context.init_display()
 
 # Create parameters
@@ -23,18 +25,33 @@ my_pnt3 = my_context.basic_operations.MakePointXYZ(p.X3,p.Y3,p.Z3, name="Pnt3") 
 my_pnt4 = my_context.basic_operations.MakePointXYZ(p.X4,p.Y4,p.Z4, name="Pnt4")   # Create the second point box2
 
 # create boxes
-box1 = my_context.prim_operations.MakeBoxTwoPnt(my_pnt1,my_pnt2,name="Box1")            # Create the box
-box2 = my_context.prim_operations.MakeBoxTwoPnt(my_pnt3,my_pnt4,name="Box2")            # Create the box
+box1 = my_context.prim_operations.MakeBoxTwoPnt(my_pnt1,my_pnt2,name="Box1", show=True)            # Create the box
+box2 = my_context.prim_operations.MakeBoxTwoPnt(my_pnt3,my_pnt4,name="Box2", show=True)            # Create the box
 
 # boolean subtract box2 from box1 
-booled_box = my_context.boolean_operations.MakeBoolean( box1, box2, 2, name='BooleanBox')
+booled_box = my_context.boolean_operations.MakeBoolean( box1, box2, 1, name='BooleanBox', show=False)
+
 
 # add fillets to the booleaned box
 fillet_box = my_context.local_operations.MakeFilletAll( booled_box, p.RADIUS, name='FilletBox', show=True)
 
-for i in range(14,40,5):
-    print 'changed parameter p.Z2 from %s to %s' % ( i-1, i ) 
+# configuring presentations
+pres1, pres2, pres_fillet = my_context.get_presentation(box1), my_context.get_presentation(box2), my_context.get_presentation(fillet_box)
+pres1.SetTransparency(.8); pres1.SetColor(12) 
+pres2.SetTransparency(.8); pres1.SetColor(12)
+pres_fillet.SetColor(1)
+
+for i in range(14,40,1):
+    print 'changed parameter p.Z2 from %s to %s' % ( i-1, i )
+    tA = time.time() 
     p.Z2 = i
-    p.RADIUS = i/10.
+    my_context.update(fillet_box)
+    print 'updating geometry took:', time.time() - tA
+
+#for i in range(10,40,5):
+#    tA = time.time() 
+#    p.Z2 = i
+#    print 'updating geometry took:', time.time() - tA
+
 
 my_context.start_display()
