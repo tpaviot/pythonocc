03f1bc1861d23a8fe49d22763c602a06975612c8
Thomas Paviot <tpaviot@gmail.com>
2009-08-16 21:47:30 +0000

    Fixed another indentation level + display warning if python-profiler is not installed

--- src/samples/Level2/PAF/test_performance.py
+++ src/samples/Level2/PAF/test_performance.py
@@ -11,7 +11,7 @@ from OCC.TPrsStd import *
 from OCC.TNaming import *
 from OCC.TDocStd import *
 
-import time
+import sys, time
 
 #===============================================================================
 # SETUP
@@ -316,18 +316,23 @@ def main():
 
 
 def profile_main():
-     # This is the main function for profiling 
-     # We've renamed our original main() above to real_main()
-     import cProfile, pstats
-     prof = cProfile.Profile()
-     prof = prof.runctx("main()", globals(), locals())
-     print "<pre>"
-     stats = pstats.Stats(prof)
-     stats.sort_stats("time")  # Or cumulative
-     stats.print_stats(80)  # 80 = how many to print
-     # The rest is optional.
-     stats.print_callees()
-     stats.print_callers()
-     print "</pre>"
+    # This is the main function for profiling 
+    # We've renamed our original main() above to real_main()
+    import cProfile
+    try:
+        import pstats
+    except ImportError:
+        print "You have to installe python-profiler"
+        sys.exit(0)
+    prof = cProfile.Profile()
+    prof = prof.runctx("main()", globals(), locals())
+    print "<pre>"
+    stats = pstats.Stats(prof)
+    stats.sort_stats("time")  # Or cumulative
+    stats.print_stats(80)  # 80 = how many to print
+    # The rest is optional.
+    stats.print_callees()
+    stats.print_callers()
+    print "</pre>"
 
 profile_main()
