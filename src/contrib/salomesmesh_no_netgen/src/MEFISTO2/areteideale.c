fc1914b601c31cd739141b0b49ec91833230aef6
Thomas Paviot <tpaviot@gmail.com>
2009-08-25 15:01:45 +0000

    Added C files generated from f2c

---
+++ src/contrib/salomesmesh_no_netgen/src/MEFISTO2/areteideale.c
@@ -0,0 +1,48 @@
+/* areteideale.f -- translated by f2c (version 20061008).
+   You must link the resulting object file with libf2c:
+	on Microsoft Windows system, link with libf2c.lib;
+	on Linux or Unix systems, link with .../path/to/libf2c.a -lm
+	or, if you install libf2c.a in a standard place, with -lf2c -lm
+	-- in that order, at the end of the command line, as in
+		cc *.o -lf2c -lm
+	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
+
+		http://www.netlib.org/f2c/libf2c.zip
+*/
+
+#include "f2c.h"
+
+/*  MEFISTO : library to compute 2D triangulation from segmented boundaries */
+
+/*  Copyright (C) 2006  Laboratoire J.-L. Lions UPMC Paris */
+
+/*  This library is free software; you can redistribute it and/or */
+/*  modify it under the terms of the GNU Lesser General Public */
+/*  License as published by the Free Software Foundation; either */
+/*  version 2.1 of the License. */
+
+/*  This library is distributed in the hope that it will be useful, */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/*  Lesser General Public License for more details. */
+
+/*  You should have received a copy of the GNU Lesser General Public */
+/*  License along with this library; if not, write to the Free Software */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA */
+
+/*  See http://www.ann.jussieu.fr/~perronne or email Perronnet@ann.jussieu.fr */
+
+
+/*  File   : areteideale.f */
+/*  Module : SMESH */
+/*  Author : Alain PERRONNET */
+/*  Date   : 13 novembre 2006 */
+/* double precision function areteideale( xyz, direction ) */
+/* Main program */ int MAIN__(void)
+{
+    static real areteideale;
+
+    areteideale = 10.f;
+    return ;
+} /* MAIN__ */
+
