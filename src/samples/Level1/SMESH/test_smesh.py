9f04bd23021646da42e1df0cae796bd904648c7a
Thomas Paviot <tpaviot@gmail.com>
2009-08-25 11:22:51 +0000

    Updated test_smesh sample

--- src/samples/Level1/SMESH/test_smesh.py
+++ src/samples/Level1/SMESH/test_smesh.py
@@ -3,11 +3,14 @@
 
 from OCC.BRepPrimAPI import *
 from OCC.TopAbs import *
+from OCC.TopExp import *
+from OCC.TopoDS import *
 from OCC.SMESH import *
 from OCC.StdMeshers import *
+from OCC.MeshVS import *
 
 #Create the shape to mesh
-my_shape = BRepPrimAPI_MakeSphere(40).Shape()
+aShape = BRepPrimAPI_MakeSphere(40).Shape()
 
 aMeshGen = SMESH_Gen()
 aMesh = aMeshGen.CreateMesh(0,True)
@@ -21,6 +24,43 @@ a2dHypothseis = StdMeshers_QuadranglePreference(2,0,aMeshGen)
 a2dAlgo = StdMeshers_Quadrangle_2D(3,0,aMeshGen)
 
 #Calculate mesh
-aMesh.ShapeToMesh(my_shape)
+aMesh.ShapeToMesh(aShape)
+
+exp3 = TopExp_Explorer(aMesh.GetShapeToMesh(), TopAbs_EDGE)
+while exp3.More():
+    print "bouzou"
+#    aMesh.AddHypothesis(exp3.Current(),0)
+    #aMesh.RebuildPropagationChains()
+    break
+
+#Assign hyptothesis to mesh
+aMesh.AddHypothesis(aShape,0)
+aMesh.AddHypothesis(aShape,1)
+aMesh.AddHypothesis(aShape,2)
+aMesh.AddHypothesis(aShape,3)
+
+#Compute the data
+aMeshGen.Compute(aMesh,aMesh.GetShapeToMesh())
+
+# Export the data
+aMesh.ExportDAT("_TEST.DAT")
+
+# Display the data
+aDS = SMESH_MeshVSLink(aMesh)
+aMeshVS = MeshVS_Mesh(True)
+DMF = 1 # to wrap!
+MeshVS_BP_Mesh       =  5 # To wrap!
+
+aPrsBuilder = MeshVS_MeshPrsBuilder(aMeshVS.GetHandle(),DMF,aDS.GetHandle(),0,MeshVS_BP_Mesh)
+aMeshVS.SetDataSource(aDS.GetHandle())
+aMeshVS.AddBuilder(aPrsBuilder.GetHandle(),True)
+
+#Create the graphic window and display the mesh
+from OCC.Display.wxSamplesGui import display, start_display
+context = display.Context
+print dir(context)
+context.Display(aMeshVS.GetHandle())
+start_display()
+
 
 
