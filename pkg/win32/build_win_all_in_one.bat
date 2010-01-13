20dd1b30ae40243c104ab59ffccbcf6f0854d04f
Thomas Paviot <tpaviot@gmail.com>
2010-01-13 04:29:54 +0000

    Added Windows build scripts

---
+++ pkg/win32/build_win_all_in_one.bat
@@ -0,0 +1,1 @@
+python setup.py build --enable-geom --enable-smesh --all_in_one -cmsvc --with-smesh-lib=Z:\Salome\salomesmesh\tags\5.1.2_v2\win32\lib --with-geom-lib=Z:\Salome\salomegeometry\tags\5.1.2_v7\win32\lib
