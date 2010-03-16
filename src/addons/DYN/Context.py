ab6ae9dd7bc7205c7e2a6022e4ecf04919b2af72
Jelle Feringa <jelleferinga@gmail.com>
2010-03-16 10:33:56 +0000

    rounding did not use an argument. so when building a box of say, 1.5, 0.5, 0.5 the box would be truncated to 1,1,1 which will segfault the ode module, since it cannot allow to create a body without mass

--- src/addons/DYN/Context.py
+++ src/addons/DYN/Context.py
@@ -20,12 +20,18 @@ class DynamicShape(ode.Body):
         self._VIEW_COG = False
         self._cog_pos = []
         self._fixed = False #by default, a 'moving' shape
+        self._max_integration_steps = 1100
         
     def store_cog_position(self,pos):
         ''' This fonction appends the cog position to a list, in order for instance
         to draw a spline from these positions, i.e. the trajectory of the point
         '''
         self._cog_pos.append(pos)
+    
+    #===========================================================================
+    # Code style: Thomas, you should use properties rather than getter / setters...
+    # an example on line:
+    #===========================================================================
     
     def get_fixed(self):
         return self._fixed
@@ -59,9 +65,9 @@ class DynamicShape(ode.Body):
         # Get intertia properties of the shape
         cog = props.CentreOfMass().XYZ()
         x_cog,y_cog,z_cog = cog.X(),cog.Y(),cog.Z()
-        x_cog = round(x_cog)
-        y_cog = round(y_cog)
-        z_cog = round(z_cog)
+        x_cog = round(x_cog,3)
+        y_cog = round(y_cog,3)
+        z_cog = round(z_cog,3)
         self.x_g = x_cog
         self.y_g = y_cog
         self.z_g = z_cog
@@ -90,6 +96,7 @@ class DynamicShape(ode.Body):
         i23 = round(i23)
  
         M = ode.Mass()
+        M.mass = 2.
         M.setParameters(mass,0,0,0,i11,i22,i33,i12,i13,i23)
         self.setMass(M)
         
@@ -103,10 +110,13 @@ class DynamicSimulationContext(ode.World):
         self._DISPLAY_INITIALIZED = False
         self._COLLISION_DETECTION = False
         self._delta_t = 0.01 #timestep default value
+        self._max_integration_steps = 1100
         # A joint group for the contact joints that are generated whenever
         # two bodies collide
         self._contactgroup = None
         self._space = None
+        # callback after each integration step
+        self.post_step_callable=None
         print 'Dynamic simulation context initialized.'
     
     def enable_gravity(self):
@@ -140,6 +150,12 @@ class DynamicSimulationContext(ode.World):
     
     def set_time_step(self,delta_t):
         self._delta_t = delta_t
+        
+    def set_integration_steps(self, value):
+        self._max_integration_steps = value
+        
+    def set_callable_post_step(self, _callable):
+        self.post_step_callable= _callable
             
     # Collision callback
     def _collision_callback(self, args, geom1, geom2):
@@ -162,12 +178,18 @@ class DynamicSimulationContext(ode.World):
     def start_open_loop(self):
         #delta_t = 0.01
         i = 0
-        while i<1100:
+        print 'number of integration steps:', self._max_integration_steps
+        while i<self._max_integration_steps:
             i = i+1
             if self._COLLISION_DETECTION: 
                 self._space.collide((self,self._contactgroup), self._collision_callback)
             # step world
             self.step(self._delta_t)
+            
+            # call post-step callable, if its there...
+            if self.post_step_callable is not None:
+                self.post_step_callable()
+            
             # remove all contact joints
             if self._COLLISION_DETECTION:
                 self._contactgroup.empty()
