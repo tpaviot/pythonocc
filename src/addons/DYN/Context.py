289409b0e992833b33efce809206b06c22b21a0e
Thomas Paviot <tpaviot@gmail.com>
2010-03-24 17:25:50 +0000

    Rolling back changes

--- src/addons/DYN/Context.py
+++ src/addons/DYN/Context.py
@@ -9,7 +9,7 @@ from OCC.TColgp import *
 from OCC.GeomAPI import *
 
 # For trimesh collision
-#from OCC.MSH.Mesh import *
+from OCC.MSH.Mesh import *
 import time
 
 from OCC.Utils.Common import get_boundingbox
@@ -190,13 +190,13 @@ class DynamicSimulationContext(ode.World):
         self._DISPLAY_INITIALIZED = False
         self._COLLISION_DETECTION = False
         self._delta_t = 0.01 #timestep default value
+        self._duration = 10.0 #default duration set to 10s
         # A joint group for the contact joints that are generated whenever
         # two bodies collide
         self._contactgroup = None
         self._space = None
         # callback after each integration step
         self._post_step_callable = []
-        self._duration = 10
         print 'Dynamic simulation context initialized.'
     
     def enable_gravity(self):
@@ -239,7 +239,7 @@ class DynamicSimulationContext(ode.World):
         self._display = display
         self._DISPLAY_INITIALIZED = True
     
-    def add_shape(self, topods_shape, enable_collision_detection=True, use_boundingbox=True, use_trimesh=False):
+    def add_shape(self, topods_shape, enable_collision_detection=False, use_boundingbox=False, use_trimesh=False):
         ''' Adds a TopoDS_Shape to the DYN context.
         
         @param topods_shape : the shape to add to the dynamic context
