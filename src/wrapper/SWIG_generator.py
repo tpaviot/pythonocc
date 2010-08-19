0efb5f11bb8d5cedf3844d2f72f0d4ce40aa091e
Thomas Paviot <tpaviot@gmail.com>
2010-08-19 16:22:39 +0000

    Added Author/Date/Revision/HeadURL keyword properties

--- src/wrapper/SWIG_generator.py
+++ src/wrapper/SWIG_generator.py
@@ -17,6 +17,12 @@
 ##
 ##You should have received a copy of the GNU General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+##
+## $Revision$
+## $Date$
+## $Author$
+## $HeadURL$
+##
 
 import os, os.path
 import sys
