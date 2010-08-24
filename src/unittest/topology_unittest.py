86065114874cabc00e3c178d07357bd560593fdf
Thomas Paviot <tpaviot@gmail.com>
2010-08-24 06:28:42 +0000

    Added 2 unittests from examples

--- src/unittest/topology_unittest.py
+++ src/unittest/topology_unittest.py
@@ -1,3 +1,25 @@
+##Copyright 2010 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU General Public License for more details.
+##
+##You should have received a copy of the GNU General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+##
+## $Revision$
+## $Date$
+## $Author$
+## $HeadURL$
+
 from OCC.BRepPrimAPI import *
 from OCC.TopoDS import *
  
@@ -6,10 +28,6 @@ from OCC.Utils.Topology import Topo, WireExplorer
 import unittest
 
 class TestTopology(unittest.TestCase):
-    
-    def setUp(self):
-        self.topo = Topo(BRepPrimAPI_MakeBox(10,10,10).Shape())
-        
     def test_nested_iteration(self):
         '''check nested looping'''
         for f in self.topo.faces():
