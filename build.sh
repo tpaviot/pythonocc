f096711d364febfe795f45872b3c54008204cb75
Henning Meyer <hmeyer@gmx.eu>
2011-10-11 06:03:35 +0200

    clean source build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#git-buildpackage -S --git-ignore-new --git-tag
-DIST=natty git-buildpackage --git-ignore-new
+git-buildpackage -S --git-ignore-new --git-tag
+#DIST=natty git-buildpackage --git-ignore-new
 
 dh clean
 git clean -fd
