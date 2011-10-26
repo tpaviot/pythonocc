e5cef80b6ab7296d06f834f646eac5c69fbb2f4f
tpaviot <tpaviot@gmail.com>
2011-10-26 17:01:25 +0200

    GEOM support improvements

--- cmake/generate_modules.cmake.py
+++ cmake/generate_modules.cmake.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../src/wrapper')
 import Modules
 
-# create Modules.cmake file
+# create OCE_Modules.cmake file
 fp = open("OCE_Modules.cmake","w")
 fp.write("LIST(APPEND OCC_MODULES\n")
 # write all module names
@@ -12,4 +12,12 @@ for module in Modules.MODULES:
 fp.write(")\n")
 fp.close()
 
-    
+# create GEOM_Modules.cmake file
+fp = open("GEOM_Modules.cmake","w")
+fp.write("LIST(APPEND GEOM_MODULES\n")
+# write all module names
+for module in Modules.SALOME_GEOM_MODULES:
+    module_name = module[0]
+    fp.write("\t%s\n"%module_name)
+fp.write(")\n")
+fp.close()
