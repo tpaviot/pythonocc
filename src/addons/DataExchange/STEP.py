24f3c11f40c90882adc659c1215f91f63bc42963
tpaviot <tpaviot@gmail.com>
2011-11-23 23:14:06 +0100

    Fixed a small bug in STEP importer that prevented using get_nbr_shapes()

--- src/addons/DataExchange/STEP.py
+++ src/addons/DataExchange/STEP.py
@@ -79,22 +79,21 @@ class STEPImporter(object):
             aReader.PrintCheckTransfer(failsonly, IFSelect_ItemsByEntity)
             for n in range(1,nbr+1):
                 ok = aReader.TransferRoot(n)
-                self.nbs = aReader.NbShapes()
-                if self.nbs == 0:
+                self._nbs = aReader.NbShapes()
+                if self._nbs == 0:
                     print "At least one shape in STEP cannot be transfered"
-                elif (nbr==1 and self.nbs==1):
+                elif (nbr==1 and self._nbs==1):
                     aResShape = aReader.Shape(1)
                     if aResShape.IsNull():
                         print "At least one shape in STEP cannot be transferred"
                     self._shapes.append(aResShape)
                 else:                    
-                    for i in range(1,self.nbs+1):
+                    for i in range(1,self._nbs+1):
                         aShape = aReader.Shape(i)
                         if aShape.IsNull():
                             print "At least one shape in STEP cannot be transferred"
                         else:
                             self._shapes.append(aShape)
-                            #B.Add(compound,aShape)
             return True
         else:
             print "Error: can't read file %s"%self._filename
