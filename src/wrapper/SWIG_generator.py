dcc7f3296aecc570dba1433fd77a43589caf787f
tpaviot <tpaviot@gmail.com>
2011-11-28 08:42:33 +0100

    Improved wrapper for != operator

--- src/wrapper/SWIG_generator.py
+++ src/wrapper/SWIG_generator.py
@@ -139,6 +139,7 @@ class ModularBuilder(object):
         self.STATIC_METHODS = []
         self.ClassDocstring = ""
         self.CLASS_WRAP_EQ = False
+        self.CLASS_WRAP_NE = False
         self.MemberfunctionDocStrings = {}
         self.ALREADY_EXPOSED = []
         self.CLASSES_TO_EXCLUDE = []
@@ -667,7 +668,19 @@ class ModularBuilder(object):
             self.CLASS_WRAP_EQ = True
             self._CURRENT_CLASS_EXPOSED_METHODS.append(to_write)
             return True
-            #raise AssertionError('this is an op')
+        #
+        # Check if operator!=
+        #
+        if ("operator!=") in function_name:
+            to_write="\t\t%extend{\n"
+            to_write+="\t\t\tbool __ne_wrapper__(%s) {\n"%str
+            to_write+="\t\t\t\tif (*self!=%s) return true;\n"%param_names[0]
+            to_write+="\t\t\t\telse return false;\n"
+            to_write+="\t\t\t}\n\t\t}\n"
+            self.fp.write(to_write)
+            self.CLASS_WRAP_NE = True
+            self._CURRENT_CLASS_EXPOSED_METHODS.append(to_write)
+            return True
         to_write += str
         if END_WITH_CONST:
             to_write += ") const;\n"
@@ -747,6 +760,7 @@ class ModularBuilder(object):
         Process class
         """
         self.CLASS_WRAP_EQ = False
+        self.CLASS_WRAP_NE = False
         # list with exposed member functions decl_strings
         CURRENT_CLASS_IS_ABSTRACT = False
         self._CURRENT_CLASS_EXPOSED_METHODS = []
@@ -867,7 +881,6 @@ class ModularBuilder(object):
                  self.write_function(mem_fun,CURRENT_CLASS_IS_ABSTRACT)
         # Add __eq__ method is needed
         if self.CLASS_WRAP_EQ:
-            print "popo"
             to_write = "\t\t%"
             to_write += "pythoncode {\n"
             to_write += "\t\tdef __eq__(self,right):\n"
@@ -875,6 +888,17 @@ class ModularBuilder(object):
             to_write += "\t\t\t\treturn self.__eq_wrapper__(right)\n"
             to_write += "\t\t\texcept:\n"
             to_write += "\t\t\t\treturn False\n"
+            to_write += "\t\t}\n"
+            self.fp.write(to_write)
+        # Add __eq__ method is needed
+        if self.CLASS_WRAP_NE:
+            to_write = "\t\t%"
+            to_write += "pythoncode {\n"
+            to_write += "\t\tdef __ne__(self,right):\n"
+            to_write += "\t\t\ttry:\n"
+            to_write += "\t\t\t\treturn self.__ne_wrapper__(right)\n"
+            to_write += "\t\t\texcept:\n"
+            to_write += "\t\t\t\treturn True\n"
             to_write += "\t\t}\n"
             self.fp.write(to_write)
         self.fp.write("\n};")
