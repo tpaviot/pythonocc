ae4c0f629e2c4e1ed336cd7e3b1722f23a8835cc
tpaviot <tpaviot@gmail.com>
2011-12-29 17:45:09 +0100

    Updated SMESH modules list

--- cmake/generate_modules.cmake.py
+++ cmake/generate_modules.cmake.py
@@ -37,3 +37,13 @@ for module in Modules.SALOME_GEOM_MODULES:
     fp.write("\t%s\n"%module_name)
 fp.write(")\n")
 fp.close()
+
+# create SMESH_Modules.cmake file
+fp = open("SMESH_Modules.cmake","w")
+fp.write("LIST(APPEND SMESH_MODULES\n")
+# write all module names
+for module in Modules.SALOME_SMESH_MODULES:
+    module_name = module[0]
+    fp.write("\t%s\n"%module_name)
+fp.write(")\n")
+fp.close()
