e265619ea4eb3a8a0c34273f0ec8477502ea453b
jf--- <your_email@youremail.com>
2012-08-01 14:33:41 +0200

    added brepcheck to types lut, useful for getting analyses on the brep shapes

--- src/addons/KBE/types_lut.py
+++ src/addons/KBE/types_lut.py
@@ -1,6 +1,7 @@
 '''
 this class abstract types
 '''
+from OCC.BRepCheck import  *
 from OCC.GeomAbs import *
 from OCC.TopoDS import *
 from OCC.TopAbs import *
@@ -126,6 +127,32 @@ _geom_types_b = [GeomAbs_Line, GeomAbs_Circle, GeomAbs_Ellipse, GeomAbs_Hyperbol
                  GeomAbs_BSplineCurve, GeomAbs_OtherCurve ]
 
 
+# TODO: make a function that generalizes this, there is absolutely no need for 2 lists to define an EnumLookup!!!
+
+def fix_formatting(_str):
+    return [i.strip() for i in _str.decode('string_escape').split(',')]
+
+_brep_check_a = fix_formatting("NoError, InvalidPointOnCurve, InvalidPointOnCurveOnSurface, InvalidPointOnSurface,\
+No3DCurve, Multiple3DCurve, Invalid3DCurve, NoCurveOnSurface,\
+InvalidCurveOnSurface, InvalidCurveOnClosedSurface, InvalidSameRangeFlag, InvalidSameParameterFlag,\
+InvalidDegeneratedFlag, FreeEdge, InvalidMultiConnexity, InvalidRange,\
+EmptyWire, RedundantEdge, SelfIntersectingWire, NoSurface,\
+InvalidWire, RedundantWire, IntersectingWires, InvalidImbricationOfWires,\
+EmptyShell, RedundantFace, UnorientableShape, NotClosed,\
+NotConnected, SubshapeNotInShape, BadOrientation, BadOrientationOfSubshape,\
+InvalidToleranceValue, CheckFail")
+
+_brep_check_b = [BRepCheck_NoError, BRepCheck_InvalidPointOnCurve, BRepCheck_InvalidPointOnCurveOnSurface, BRepCheck_InvalidPointOnSurface,
+BRepCheck_No3DCurve, BRepCheck_Multiple3DCurve, BRepCheck_Invalid3DCurve, BRepCheck_NoCurveOnSurface,
+BRepCheck_InvalidCurveOnSurface, BRepCheck_InvalidCurveOnClosedSurface, BRepCheck_InvalidSameRangeFlag, BRepCheck_InvalidSameParameterFlag,
+BRepCheck_InvalidDegeneratedFlag, BRepCheck_FreeEdge, BRepCheck_InvalidMultiConnexity, BRepCheck_InvalidRange,
+BRepCheck_EmptyWire, BRepCheck_RedundantEdge, BRepCheck_SelfIntersectingWire, BRepCheck_NoSurface,
+BRepCheck_InvalidWire, BRepCheck_RedundantWire, BRepCheck_IntersectingWires, BRepCheck_InvalidImbricationOfWires,
+BRepCheck_EmptyShell, BRepCheck_RedundantFace, BRepCheck_UnorientableShape, BRepCheck_NotClosed,
+BRepCheck_NotConnected, BRepCheck_SubshapeNotInShape, BRepCheck_BadOrientation, BRepCheck_BadOrientationOfSubshape,
+BRepCheck_InvalidToleranceValue, BRepCheck_CheckFail]
+
+brepcheck_lut = EnumLookup( _brep_check_a, _brep_check_b )
 curve_lut = EnumLookup(_curve_typesA,_curve_typesB)
 surface_lut = EnumLookup(_surface_typesA, _surface_typesB)
 state_lut = EnumLookup(_stateA, _stateB)
