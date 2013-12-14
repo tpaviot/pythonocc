5a04b1239bc7f4f2d51f8a0d2854b6c6eb243b6a
tpaviot <tpaviot@gmail.com>
2013-12-14 11:41:45 +0100

    Cleaned up the DYN package

--- src/addons/DYN/Joints.py
+++ src/addons/DYN/Joints.py
@@ -1,3 +1,5 @@
+##Copyright 2009-2013 Thomas Paviot (tpaviot@gmail.com)
+##
 ##This file is part of pythonOCC.
 ##
 ##pythonOCC is free software: you can redistribute it and/or modify
@@ -12,76 +14,82 @@
 ##
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
-# $Revision: 1081 $
-# $Date: 2010-08-19 18:22:39 +0200 (Thu, 19 Aug 2010) $
-# $Author: tpaviot $
-# $HeadURL: https://pythonocc.googlecode.com/svn/trunk/src/addons/DYN/Context.py $
 
 # The DYN joints are dervied from ode joints
+
+import os
+
 import ode
 
 from OCC.gp import *
 
+
 class BaseJoint(object):
     ''' Base class for all joints
     '''
-    def set_anchor(self,gp_point_center):
+    def set_anchor(self, gp_point_center):
         ''' Set the anchor of the ball joint
         '''
         self.setAnchor(gp_point_center.Coord())
 
-    def attach_shapes(self,shape1,shape2):
-        self.attach(shape1,shape2)
+    def attach_shapes(self, shape1, shape2):
+        self.attach(shape1, shape2)
 
     def set_axis(self, gp_direction):
         ''' sets the hinge axis, which is a gp_Dir
         '''
         self.setAxis(gp_direction.Coord())
 
+
 class DynamicHingeJoint(ode.HingeJoint, BaseJoint):
     """ Create a hinge joint between 2 dynamic shapes
     """
-    def __init__(self,dyn_context):
+    def __init__(self, dyn_context):
         self.dyn_context = dyn_context
-        ode.HingeJoint.__init__(self,dyn_context)
+        ode.HingeJoint.__init__(self, dyn_context)
+
 
 class DynamicUniversalJoint(ode.HingeJoint, BaseJoint):
     """ Create a hinge joint between 2 dynamic shapes
     """
-    def __init__(self,dyn_context):
+    def __init__(self, dyn_context):
         self.dyn_context = dyn_context
-        ode.UniversalJoint.__init__(self,dyn_context)
+        ode.UniversalJoint.__init__(self, dyn_context)
+
 
 class DynamicSliderJoint(ode.SliderJoint, BaseJoint):
     """ Create a slider joint between 2 dynamic shapes
     """
-    def __init__(self,dyn_context):
+    def __init__(self, dyn_context):
         self.dyn_context = dyn_context
         ode.SliderJoint.__init__(self, dyn_context)
-        #del self.set_anchor # not present with a slider joint
+
 
 class DynamicBallJoint(ode.BallJoint, BaseJoint):
     """ Create a joint between 2 DynamicShapes
     """
-    def __init__(self,dyn_context):
+    def __init__(self, dyn_context):
         self.dyn_context = dyn_context
-        ode.BallJoint.__init__(self,dyn_context)
+        ode.BallJoint.__init__(self, dyn_context)
 
-import os
 
 def mem(size="rss"):
     """Generalization; memory sizes: rss, rsz, vsz."""
     return int(os.popen('ps -p %d -o %s | tail -1' %
                         (os.getpid(), size)).read())
+
+
 def rss():
     """Return ps -o rss (resident) memory in kB."""
     return float(mem("rss"))/1024
+
+
 def print_consumed_memory():
-    print 'memory consumption for current pid: %fMb'%(rss()-initial_memory)
-initial_memory = rss()
-print initial_memory
+    print 'memory consumption for current pid: %fMb' % (rss()-initial_memory)
 
 if __name__ == '__main__':
+    initial_memory = rss()
+    print initial_memory
     # simple example : create 2 boxes connected with a ball joint
     from Shape import DynamicShape
     from Context import DynamicSimulationContext
@@ -89,15 +97,15 @@ if __name__ == '__main__':
     from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox
     from OCC.Display.SimpleGui import *
     display, start_display, add_menu, add_function_to_menu = init_display()
-    
+
     # Create the context
     dyn_context = DynamicSimulationContext()
     dyn_context.set_display(display, safe_yield)
     # Create s shape and add it to the dynamic context
-    s1 = BRepPrimAPI_MakeBox(10,20,30).Shape()
+    s1 = BRepPrimAPI_MakeBox(10, 20, 30).Shape()
     d1 = dyn_context.register_shape(s1)
     # Create s shape and add it to the dynamic context
-    s2 = BRepPrimAPI_MakeBox(30,10,20).Shape()
+    s2 = BRepPrimAPI_MakeBox(30, 10, 20).Shape()
     d2 = dyn_context.register_shape(s2)
     # Create the ball joint
     #j = DynamicBallJoint(dyn_context)
@@ -105,38 +113,34 @@ if __name__ == '__main__':
     #j.attach_shapes(d1, d2)
     #j.set_anchor(ball_center)
     #dyn_context.register_joint(j)
-    
+
     # Create the hinge joint
     j = DynamicHingeJoint(dyn_context)
-    j.attach_shapes(d1,d2)# = DynamicHingeJoint(d1.get_parent_context(),d2)
+    j.attach_shapes(d1, d2)  # = DynamicHingeJoint(d1.get_parent_context(),d2)
     #dyn_context)
     #j.attach_shapes(d1,d2)
-    hinge_center = gp_Pnt(5,4,3)
-    hinge_direction = gp_Dir(1,0,0)
+    hinge_center = gp_Pnt(5, 4, 3)
+    hinge_direction = gp_Dir(1, 0, 0)
     j.set_anchor(hinge_center)
     j.set_axis(hinge_direction)
     dyn_context.register_joint(j)
     # Sets initial conditions
-    d1.setAngularVel([-1,0,0])
+    d1.setAngularVel([-1, 0, 0])
     dyn_context.register_post_step_callback(safe_yield)
     #
     # Create a function to check the memory consumption
     # Displays the number of garbage collected objects
     i = 0
+
     def t():
         global i
         i += 1
-        print "Simulation STEP : %i"%i
-        print "\tnumber of transients : %i"%len(GarbageCollector.garbage._transients)
-        print "\tnumber of handles : %i"%len(GarbageCollector.garbage._handles)
-        print "\tnumber of misc : %i"%len(GarbageCollector.garbage._misc)
+        print "Simulation STEP : %i" % i
+        print "\tnumber of transients : %i" % len(GarbageCollector.garbage._transients)
+        print "\tnumber of handles : %i" % len(GarbageCollector.garbage._handles)
+        print "\tnumber of misc : %i" % len(GarbageCollector.garbage._misc)
         print_consumed_memory()
-
-        
         #print GarbageCollector.garbage
         # register this call back
     dyn_context.register_post_step_callback(t)
     dyn_context.start_open_loop()
-            
-if __name__ == '__main__':
-    w = ode.World()
--- src/addons/DYN/Shape.py
+++ src/addons/DYN/Shape.py
@@ -1,3 +1,5 @@
+##Copyright 2009-2013 Thomas Paviot (tpaviot@gmail.com)
+##
 ##This file is part of pythonOCC.
 ##
 ##pythonOCC is free software: you can redistribute it and/or modify
@@ -12,10 +14,6 @@
 ##
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
-# $Revision: 1081 $
-# $Date: 2010-08-19 18:22:39 +0200 (Thu, 19 Aug 2010) $
-# $Author: tpaviot $
-# $HeadURL: https://pythonocc.googlecode.com/svn/trunk/src/addons/DYN/Context.py $
 
 # The DYN joints are dervied from ode joints
 import ode
@@ -34,91 +32,95 @@ from OCC.BRepGProp import *
 # Precision
 EPSILON = 1e-8
 
+
 class DynamicShape(ode.Body):
     """ Defines a body attached to a TopoDS_Shape to be used
     in a simulation context
     """
     def __init__(self, dyn_context, infinite_rotation=False):
         ''' function prototype:
-        DynamicShape(self, parent_context, topods_shape, enable_collision_detection,use_boundingbox,use_trimesh )
+        DynamicShape(self, parent_context, topods_shape,
+        enable_collision_detection,use_boundingbox,use_trimesh )
         '''
-        ode.Body.__init__(self,dyn_context)
+        ode.Body.__init__(self, dyn_context)
 
         self._parent_context = dyn_context
         self._shape = None
-        self._ais_shape = None #when the shape is displayed
+        self._ais_shape = None  # when the shape is displayed
         self._VIEW_COG = False
         self._inf_rotation = infinite_rotation
         self._cog_pos = []
-       
-        self._fixed = False #by default, a 'moving' shape
-        self._enable_collision_detection = False #By default, collision detection is disabled for this shape
-        self._use_boundingbox = False #by default
-        self._use_trimesh = False #by default
+
+        self._fixed = False  # by default, a 'moving' shape
+        self._enable_collision_detection = False  # By default, collision detection is disabled for this shape
+        self._use_boundingbox = False  # by default
+        self._use_trimesh = False  # by default
         self._collision_geometry = None
-    
+
     def __del__(self):
         self._shape = None
         self._ais_shape = None
         self._parent_context = None
         print 'shape deleted'
 
-    #def __deepcopy__(self, memo):
-    #    '''deepcooooopy'''
-        #import ipdb; ipdb.set_trace()
-
     def get_parent_context(self):
         ''' returns the parent dynamic context
         '''
         return self._parent_context
-    
+
     def enable_collision_detection(self):
         ''' tells this shape to be computed in the collision detection
         '''
         self._enable_collision_detection = True
         self._space = self._parent_context.get_collision_space()
-    
+
     def use_bounding_box(self):
-        xmin,ymin,zmin, xmax,ymax,zmax  = get_boundingbox(self._shape, EPSILON)
-        dx,dy,dz = xmax-xmin, ymax-ymin, zmax-zmin
-        self._collision_geometry = ode.GeomBox(self._space, lengths=(dx,dy,dz))
+        xmin, ymin, zmin, xmax, ymax, zmax = get_boundingbox(self._shape, EPSILON)
+        dx, dy, dz = xmax-xmin, ymax-ymin, zmax-zmin
+        self._collision_geometry = ode.GeomBox(self._space, lengths=(dx, dy, dz))
         self._collision_geometry.setBody(self)
-    
+
     def use_sphere(self):
         ''' Connect a ode.GeomSphere to the body
-        The sphere radius is computed as the mean of the bounding box dimensions. As a consequence,
-        if the TopoDS_Shape is a sphere, then the radius of the ode.GeomSphere will be the radius
+        The sphere radius is computed as the mean of the bounding box
+        dimensions. As a consequence,
+        if the TopoDS_Shape is a sphere, then the radius of the ode.GeomSphere
+        will be the radius
         of the sphere
         '''
-        xmin,ymin,zmin, xmax,ymax,zmax = get_boundingbox(self._shape, EPSILON)
-        dx,dy,dz = xmax-xmin, ymax-ymin, zmax-zmin
-        r = (dx+dy+dz)/6.# /3 gives the mean diameter, so divide by 2 for the radius
-        self._collision_geometry = ode.GeomSphere(self._space, radius = r)
+        xmin, ymin, zmin, xmax, ymax, zmax = get_boundingbox(self._shape, EPSILON)
+        dx, dy, dz = xmax-xmin, ymax-ymin, zmax-zmin
+        r = (dx+dy+dz)/6.  # /3 gives the mean diameter, so divide by 2 for the radius
+        self._collision_geometry = ode.GeomSphere(self._space, radius=r)
         self._collision_geometry.setBody(self)
-    
+
     def use_trimesh(self):
         self._compute_trimesh()
-        
+
     def get_fixed(self):
         ''' Returns true if the body is fixed
         '''
         return self._fixed
-    
+
     def set_fixed(self):
         ''' Tells ode to not take this body into account. It will then be out of the simulation
         loop and remain fixed during the simulation
         '''
         self.disable()
         self._fixed = True
-            
-    def _compute_trimesh(self,quality_factor=1.0):
-        ''' Connect a ode.Trimesh to this body. The mesh is generated from the MSH subpackage. vertices lits
+
+    def _compute_trimesh(self, quality_factor=1.0):
+        ''' Connect a ode.Trimesh to this body. The mesh is generated from the
+        MSH subpackage.
+        @TODO use the tesselation modules vertices lits
         and faces indices are passed to the trimesh.
         The default mesh precision is divided by the quality factor:
-        - if quality_factor>1, the mesh will be more precise, i.e. more triangles (more precision, but also
+        - if quality_factor>1, the mesh will be more precise, i.e. more
+        triangles (more precision, but also
         more memory consumption and time for the mesher,
         - if quality_factor<1, the mesh will be less precise.
-        By default, this argument is set to 1 : the default precision of the mesher is used. 
+        By default, this argument is set to 1 : the default precision of the
+        mesher is used.
         '''
         a_mesh = QuickTriangleMesh()
         a_mesh.set_shape(self._shape)
@@ -129,78 +131,81 @@ class DynamicShape(ode.Body):
         # The results of the mesh computation are passed to the trimesh manager
         #a_mesh.build_lists()
         vertices = a_mesh.get_vertices()
-        # Before we set the vertices list to the trimesh, we must translate them:
+        # Before we set the vertices list to the trimesh, we must translate
+        # them:
         # TODO : there might be a simplier way to achieve that point
         for i in range(len(vertices)):
             v = vertices[i]
             v_x = v[0] - self.x_g
             v_y = v[1] - self.y_g
             v_z = v[2] - self.z_g
-            vertices[i] = [v_x,v_y,v_z]
+            vertices[i] = [v_x, v_y, v_z]
         # faces also have to be reshaped
         faces = a_mesh.get_faces()
-        i=0
-        f=[]
-        while i<len(faces):
-            f.append([i,i+1,i+2])
-            i=i+3
+        i = 0
+        f = []
+        while i < len(faces):
+            f.append([i, i+1, i+2])
+            i = i+3
         # Create the trimesh data
         td = ode.TriMeshData()
-        td.build(vertices,f)
-        self._collision_geometry = ode.GeomTriMesh(td,self._parent_context.get_collision_space())
+        td.build(vertices, f)
+        self._collision_geometry = ode.GeomTriMesh(td, self._parent_context.get_collision_space())
         self._collision_geometry.setBody(self)
-           
-    def set_ais_shape(self,ais_shape):
+
+    def set_ais_shape(self, ais_shape):
         self._ais_shape = ais_shape
- 
+
     def get_ais_shape(self):
         return self._ais_shape
-    
+
     def get_shape(self):
         return self._shape
-    
+
     def set_shape(self, topods_shape):
         ''' This method builds a DynamicShape from a TopoDS_Shape.
         '''
         self._shape = topods_shape
         props = GProp_GProps()
-        BRepGProp_VolumeProperties(self._shape,props)
+        BRepGProp_VolumeProperties(self._shape, props)
         # Get inertia properties of the shape
         cog = props.CentreOfMass().XYZ()
-        x_cog,y_cog,z_cog = cog.X(),cog.Y(),cog.Z()
-        comma=3
-        x_cog = round(x_cog,comma)
-        y_cog = round(y_cog,comma)
-        z_cog = round(z_cog,comma)
+        x_cog, y_cog, z_cog = cog.X(), cog.Y(), cog.Z()
+        comma = 3
+        x_cog = round(x_cog, comma)
+        y_cog = round(y_cog, comma)
+        z_cog = round(z_cog, comma)
         self.x_g = x_cog
         self.y_g = y_cog
         self.z_g = z_cog
         #
         # Set position of the body
         #
-        self.setPosition([x_cog,y_cog,z_cog])
+        self.setPosition([x_cog, y_cog, z_cog])
         #
         # Get inertia properties of the TopoDS_Shape
         #
         mass = props.Mass()
 
         inertia_matrix = props.MatrixOfInertia()
-        i11 = inertia_matrix.Value(1,1)
-        i22 = inertia_matrix.Value(2,2)
-        i33 = inertia_matrix.Value(3,3)
-        i12 = inertia_matrix.Value(1,2)
-        i13 = inertia_matrix.Value(1,3)
-        i23 = inertia_matrix.Value(2,3)
+        i11 = inertia_matrix.Value(1, 1)
+        i22 = inertia_matrix.Value(2, 2)
+        i33 = inertia_matrix.Value(3, 3)
+        i12 = inertia_matrix.Value(1, 2)
+        i13 = inertia_matrix.Value(1, 3)
+        i23 = inertia_matrix.Value(2, 3)
         # TODO : is it really necessary to round these values?
         # let me answer that question: a definitive, absolute 100% clear, *NO*
         M = ode.Mass()
-        M.setParameters(mass,0,0,0,i11,i22,i33,i12,i13,i23)
+        M.setParameters(mass, 0, 0, 0, i11, i22, i33, i12, i13, i23)
         self.setMass(M)
 
         if not self._inf_rotation:
-            #===============================================================================
-            #    this is a common source of error, so defaulting to the finite rotation
-            #    that avoids many cases of the common "bNormalizationResult" error
+            #===============================================================
+            #    this is a common source of error, so defaulting to the finite
+            #   rotation
+            #    that avoids many cases of the common "bNormalizationResult"
+            #  error
             #    a more in-depth explanation from the ODE docs:
             #
             #    - 0: An "infinitesimal" orientation update is used. This is
@@ -217,4 +222,3 @@ class DynamicShape(ode.Body):
             #
             #===============================================================================
             self.setFiniteRotationMode(1)
-
