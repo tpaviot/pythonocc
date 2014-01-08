3c2d248bfcf51910a813624e934335c4e9543d07
Jelle Feringa <jelleferinga@gmail.com>
2014-01-08 11:08:06 +0100

    clean up of the example, closing issue in github

--- src/examples/GEOM/partition.py
+++ src/examples/GEOM/partition.py
@@ -1,9 +1,11 @@
+from OCC.Display.SimpleGui import init_display
+
 __author__ = 'jelle'
 
 import functools
 
 from OCC.Utils.Construct import *
-from OCC import Partition
+from OCC.Partition import *
 from OCC.TopAbs import *
 from OCC.TopoDS import *
 from OCC.Utils.Topology import Topo
@@ -30,29 +32,28 @@ pl2 = make_closed_polygon(gp_Pnt(0,-10,-10),
 
 f1 = make_face(pl1)
 f2 = make_face(pl2)
-disp = Display()
-disp.display.DisplayShape([f1,f2])
-disp.display.FitAll()
-disp.start_display()
 
-import ipdb; ipdb.set_trace()
-
-
-spl = Partition.Partition_Spliter()
+spl = Partition_Spliter()
 spl.AddShape(f1)
 spl.AddTool(f2)
-spl.Compute()
+spl.Compute(TopAbs_SHAPE)
 shp = spl.Shape()
 
+# start gui
+display, start_display, add_menu, add_function_to_menu = init_display()
+
+
+# move the results a little such that we can clearly see it
 shp = translate_topods_from_vector(shp, gp_Vec(30,0,0))
 
-
+# add the result to the viewer, in black
 tp = Topo(shp)
-
 for i in tp.edges():
-    disp(i)
-disp((pl1,pl2))
+    display.DisplayColoredShape(i, "BLACK")
 
-print spl
+display.DisplayShape((pl1,pl2))
+display.DisplayShape((f1,f2))
 
+display.FitAll()
 
+start_display()
